e38a72e0e0c0914b040ea1c8aec8fed604c07d0b
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-05-19 10:44:21 +0300

    Update lessonPlan.js

--- routes/lessonPlan.js
+++ routes/lessonPlan.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { generateLessonPlan } = require('../controllers/lessonPlanController');
+const { generateQuizByTopic, generateQuizFromPresentation } = require('../controllers/quizController');
 
+// Планы уроков
 router.post('/generate', generateLessonPlan);
+
+// Тесты
+router.post('/quiz/generate', generateQuizByTopic);
+router.post('/quiz/from-presentation', generateQuizFromPresentation);
 
 module.exports = router;
